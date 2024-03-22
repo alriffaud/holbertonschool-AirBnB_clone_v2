f5e36c9ea06c6e858d67b832fa1dbb7d193203fc
Kiwizacxs <brian.deleon2016@outlook.com>
2024-03-22 18:47:21 +0000

    update city and state

--- models/city.py
+++ models/city.py
@@ -5,5 +5,6 @@ from models.base_model import BaseModel
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    state_id = ""
-    name = ""
+    __tablename__ 'cities'
+    state_id = Column(str(60), nullable=False, ForeingKey=("states.id"))
+    name = Column(str(129), nullable=False)
--- models/state.py
+++ models/state.py
@@ -1,8 +1,17 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    __tablename__ = 'states'
+    name = Column(str(128), nullable=False)
+    cities = relationship("City", backref="state",
+                          cascade="all, delete-orphan")
+
+    def cities(self):
+        """ returns the list of City instances with state_id equals
+        to the current State.id"""
+
