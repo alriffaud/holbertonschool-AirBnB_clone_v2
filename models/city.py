e3ec650624f31d7f69dee97255c2055482fedc52
Alberto Riffaud <alriffaud@gmail.com>
2024-03-22 16:11:49 -0300

    Modification of city.py file.

--- models/city.py
+++ models/city.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 """ City Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String, ForeignKey
 
 
 class City(BaseModel):
     """ The city class, contains state ID and name """
-    __tablename__ 'cities'
+    __tablename__ = 'cities'
+
     state_id = Column(str(60), nullable=False, ForeingKey=("states.id"))
-    name = Column(str(129), nullable=False)
+    name = Column(str(128), nullable=False)
