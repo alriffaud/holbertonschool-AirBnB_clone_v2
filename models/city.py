e06565a2c5f00fea51e48e3911865aa734f70158
Kiwizacxs <brian.deleon2016@outlook.com>
2024-03-23 20:05:33 +0000

    fix

--- models/city.py
+++ models/city.py
@@ -9,7 +9,7 @@ from models.state import State
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'cities'
+        __tablename__ = "cities"
         state_id = Column(String(60), ForeignKey(State.id), nullable=False)
         name = Column(String(128), nullable=False)
     else:
--- models/user.py
+++ models/user.py
@@ -1,12 +1,21 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
+from sqlalchemy import Column, ForeignKey,  String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    __tablename__ = 'users'
-    email = Column(str(128), nullable=False)
-    password = Column(str(128), nullable=False)
-    first_name = Column(str(128), nullable=False)
-    last_name = Column(str(128), nullable=False)
+    __tablename__ = "users"
+    if getenv("HBTN_TYPE_STORAGE") = "db":
+        email = Column(str(128), nullable=False)
+        password = Column(str(128), nullable=False)
+        first_name = Column(str(128), nullable=False)
+        last_name = Column(str(128), nullable=False)
+    else:
+        email = ""
+        password = ""
+        first_name = ""
+        last_name = ""
