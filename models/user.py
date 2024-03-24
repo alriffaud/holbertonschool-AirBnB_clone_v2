51599cd922166ca0720b568d620a5a02ab52d3c7
Kiwizacxs <brian.deleon2016@outlook.com>
2024-03-24 04:24:01 +0000

    fix

--- models/user.py
+++ models/user.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-#from sqlalchemy import Column, ForeignKey, String
-#from sqlalchemy.orm import relationship
-#from os import getenv
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+from os import getenv
 
 
 class User(BaseModel):
