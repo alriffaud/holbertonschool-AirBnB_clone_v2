0e75d749599ec721bfa5a9d0efa05a2fd353eae5
Kiwizacxs <brian.deleon2016@outlook.com>
2024-03-24 03:08:52 +0000

    fix

--- models/state.py
+++ models/state.py
@@ -1,29 +1,29 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
-from os import getenv
+from models.base_model import BaseModel
+#from sqlalchemy import Column, String
+#from sqlalchemy.orm import relationship
+#from os import getenv
 
 
-class State(BaseModel, Base):
+class State(BaseModel):
     """ State class """
-    if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = "states"
-        name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
-                              cascade="all, delete-orphan")
-    else:
-        name = ""
-
-        @property
-        def cities(self):
-            """ returns the list of City instances with state_id equals
-            to the current State.id"""
-            from models import storage
-            from models.city import City
-            city_instances = []
-            for obj in storage.all(City).values():
-                if obj.state_id == self.id:
-                    city_instances.append(obj)
-            return city_instances
+    #if getenv('HBNB_TYPE_STORAGE') == 'db':
+        #__tablename__ = "states"
+       # name = Column(String(128), nullable=False)
+      #  cities = relationship("City", backref="state",
+     #                         cascade="all, delete-orphan")
+    #else:
+    name = ""
+#
+       # @property
+        #def cities(self):
+         #   """ returns the list of City instances with state_id equals
+          #  to the current State.id"""
+           # from models import storage
+            #from models.city import City
+            #city_instances = []
+            #for obj in storage.all(City).values():
+             #   if obj.state_id == self.id:
+             #       city_instances.append(obj)
+            #return city_instances
