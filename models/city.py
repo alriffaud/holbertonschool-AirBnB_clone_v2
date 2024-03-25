91be57eb57cd83d6c84e0786d2d524df241d62cb
Alberto Riffaud <alriffaud@gmail.com>
2024-03-25 12:16:00 -0300

    Modification of city.py and state.py files.

--- models/city.py
+++ models/city.py
@@ -11,9 +11,10 @@ class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
-        state_id = Column(String(60), ForeignKey(State.id), nullable=False)
+        state_id = Column(String(60), ForeingKey=(State.id), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place")
+        places = relationship("Place", backref="cities",
+                              cascade="all, delete-orphan")
     else:
         state_id = ""
         name = ""
