cf5a670eab870c60b17fa8c6afee82d63f7edae1
Alberto Riffaud <alriffaud@gmail.com>
2024-03-26 14:48:19 -0300

    Modification of amenity.py file.

--- models/amenity.py
+++ models/amenity.py
@@ -4,12 +4,12 @@ from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
-from models.place import place_amenity
 
 
 class Amenity(BaseModel):
     """class amenity that conatins name """
     if getenv("HBNB_TYPE_STORAGE") == "db":
+        from models.place import place_amenity
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
         place_amenities = relationship("Place", secondary=place_amenity,
