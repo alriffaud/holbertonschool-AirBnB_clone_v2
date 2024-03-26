bf7fadcf08ecba527425d76e94d7db4377baf236
Alberto Riffaud <alriffaud@gmail.com>
2024-03-26 14:09:43 -0300

    Modification of console.py and state.py files.

--- models/state.py
+++ models/state.py
@@ -9,9 +9,9 @@ from os import getenv
 class State(BaseModel, Base):
     """ State class """
     if getenv('HBNB_TYPE_STORAGE') == 'db':
-        __tablename__ = 'states'
+        __tablename__ = "states"
         name = Column(String(128), nullable=False)
-        cities = relationship("City", backref="state",
+        cities = relationship("City", backref="states",
                               cascade="all, delete-orphan")
     else:
         name = ""
