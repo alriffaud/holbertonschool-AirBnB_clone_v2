e8cb186392f06c0868ac2af420f3184b7731aa4c
Alberto Riffaud <alriffaud@gmail.com>
2024-03-28 17:55:44 -0300

    Modification of city.py and places.py files.

--- models/city.py
+++ models/city.py
@@ -3,7 +3,6 @@
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
 from os import getenv
-from models.state import State
 from sqlalchemy.orm import relationship
 
 
