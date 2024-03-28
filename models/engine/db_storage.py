51223fa651f81d34bef38846dbef04b9afaa60a6
Alberto Riffaud <alriffaud@gmail.com>
2024-03-28 13:42:42 -0300

    Modification of db_storage.py, city.py, place.py and user.py files.

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -33,7 +33,7 @@ class DBStorage():
 
     def all(self, cls=None):
         """Query on the current database session"""
-        classes = {'State': State, 'City': City, 'User': User}
+        classes = {'State': State, 'City': City, 'User': User, 'Place': Place}
         dic = {}
         if cls is None:
             for cls_name, cls_type in classes.items():
