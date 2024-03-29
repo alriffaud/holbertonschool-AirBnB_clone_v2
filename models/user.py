9b506c55affa7b4c846c04927d3bec39d0d6ef7b
Kiwizacxs <brian.deloen2016@outlook.com>
2024-03-29 00:20:21 +0000

    add model/review

--- models/user.py
+++ models/user.py
@@ -16,8 +16,8 @@ class User(BaseModel, Base):
         last_name = Column(String(128), nullable=True)
         places = relationship("Place", backref="user",
                               cascade="all, delete-orphan")
-        # review = relationship("Review", backref="user",
-        #                         cascade="all, delete-orphan")
+        review = relationship("Review", backref="user",
+                              cascade="all, delete-orphan")
     else:
         email = ""
         password = ""
