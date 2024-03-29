30f2e18635ae5aacb41c825120dcf25d1761b433
Kiwizacxs <brian.deloen2016@outlook.com>
2024-03-29 00:33:35 +0000

    update place and add amenity

--- models/amenity.py
+++ models/amenity.py
@@ -3,7 +3,7 @@
 from models.base_model import BaseModel
 from os import getenv
 from sqlalchemy import Column, String
-#from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship
 
 
 class Amenity(BaseModel):
@@ -12,7 +12,7 @@ class Amenity(BaseModel):
         from models.place import place_amenity
         __tablename__ = "amenities"
         name = Column(String(128), nullable=False)
-        #place_amenities = relationship("Place", secondary=place_amenity,
-         #                              back_populates="amenities")
+        place_amenities = relationship("Place", secondary=place_amenity,
+                                       back_populates="amenities")
     else:
         name = ""
