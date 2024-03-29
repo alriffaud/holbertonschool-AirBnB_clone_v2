368df717c61edcc3ca13b90ce8f8a12bd2d7dd9b
Kiwizacxs <brian.deloen2016@outlook.com>
2024-03-29 00:34:55 +0000

    update place and add amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 from os import getenv
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 
-class Amenity(BaseModel):
+class Amenity(BaseModel, Base):
     """class amenity that conatins name """
     if getenv("HBNB_TYPE_STORAGE") == "db":
         from models.place import place_amenity
